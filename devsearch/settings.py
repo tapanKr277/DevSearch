5b34070b1a11b1a7a0bb292f52764a5f2a377b2b
Tapan Kumar <tapankr7898@gmail.com>
2023-04-22 13:10:44 +0530

    added apis

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -128,16 +128,7 @@ WSGI_APPLICATION = 'devsearch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'devsearch',
-#         'USER':'postgres',
-#         'PASSWORD':'Alkabir@123',
-#         'HOST':'localhost',
-#         'PORT':'5432',
-#     }
-# }
+
 
 DATABASES = {
     'default': {
